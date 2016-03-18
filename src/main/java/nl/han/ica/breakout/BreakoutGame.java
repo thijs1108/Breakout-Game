8d7e9e4c2dbf5a883945f43a407c03913e0a19d9
thijs1108 <t.beltman@hotmail.nl>
2016-03-18 13:46:37 +0100

    add BreakoutGame extends GameEngine

---
+++ src/main/java/nl/han/ica/breakout/BreakoutGame.java
@@ -0,0 +1,24 @@
+package nl.han.ica.breakout;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+
+public class BreakoutGame extends GameEngine{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setupGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
